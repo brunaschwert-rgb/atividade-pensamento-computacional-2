4c3b079bfb13e49844c05da8bda025e46d9478e6
brunaschwert-rgb <bruna.schwert@amf.edu.br>
2026-04-09 22:27:31 -0300

    Create botaoaula05exercicios06.py

---
+++ botaoaula05exercicios06.py
@@ -0,0 +1,25 @@
+Primeiramente irei fazer que após pressionar o botão 1 irá girar a porteira esquerda , e depois irei  pressionar o botão 2 irá girar porteira direita e assim logo em seguida irei fazer com que os leds entram em sincronia na ordem correta sobre a escrita pedida na atividade e sendo farei com que o código funciona.
+
+No início						
+girar pino 9 =0
+girar pino 8=0
+sempre
+botão 1 pino 10
+botão 1= 1
+girar pino 9= 90 graus
+5 segundos
+botão 2 pino 11
+botão 2 =1
+girar pino 8 = 90 graus
+5 segundos
+botão 3 pino 12
+botão 3 =1 
+girar pino 9 = 180 graus
+girar pino 8 =180 graus
+5 segundos 
+botão 4 pino 13
+botão 4= 1
+girar pino 9 = 90 graus
+girar pino 9 = 0 graus
+girar pino 8 = 90 graus
+girar pino 8 = 0 graus
